03772d5ab9fbfdc77e2a8fb3cf7818154b2858e0
OrGalili <orgalil21@gmail.com>
2019-05-08 15:19:50 +0300

    Added stepout, continue buttons and functionality and validating if exefile loaded

--- src/sample/GDBController.java
+++ src/sample/GDBController.java
@@ -25,8 +25,22 @@ public class GDBController {
     @FXML
     public MenuItem openFile;
 
+    @FXML
+    public Button RunBtn;
 
-    public GDBController() {
+    @FXML
+    public Button StepBtn;
+
+    @FXML
+    public Button StepInBtn;
+
+    @FXML
+    public Button StepOutBtn;
+
+    @FXML
+    public Button ContinueBtn;
+
+    public GDBController() throws Exception {
         this.model = new GDBModel();
         model.GDBStart();
         model.setInput("set listsize unlimited");
@@ -38,69 +52,79 @@ public class GDBController {
     {
         FileChooser f = new FileChooser();
         File file = f.showOpenDialog(((MenuItem)event.getTarget()).getParentPopup().getOwnerWindow());
-        System.out.println(file.getAbsolutePath());
-        model.setInput("file "+file.getAbsolutePath());
-        model.getOutPut();
+        String exeFilePath = file.getAbsolutePath();
+        System.out.println(exeFilePath);
+//        String absolutePath = "'"+file.getAbsolutePath()+"'";
+        model.setInput("file '"+exeFilePath+"'");
+        String loadingExeFileMessage = model.getOutPut();
+        loadingExeFileMessage = loadingExeFileMessage.substring(0,loadingExeFileMessage.indexOf('\n'));
 
-        model.setInput("info sources");
-        Collection<String> linesSources = new ArrayList(Arrays.asList(model.getOutPut().split("\n")));
-        ((ArrayList<String>) linesSources).remove(0);
-        ((ArrayList<String>) linesSources).remove(0);
-        linesSources.remove("Source files for which symbols will be read in on demand:");
-        linesSources.remove("(gdb) ");
-        for(String ls:linesSources){
-            String[] filePaths = ls.split(",");
-            for (String fp : filePaths) {
-                if (fp.length()>1 && !fp.endsWith(".h")) {
-                    Tab tab = new Tab();
-                    String fileName = fp.substring(fp.lastIndexOf("/")+1);
-                    Tooltip tt = new Tooltip();
-                    tt.setText(fp);
-                    tab.setTooltip(tt);
-                    tab.setText(fileName);
+        if(loadingExeFileMessage.equals("Reading symbols from "+exeFilePath+"...done.")) {
+            model.setInput("info sources");
+            Collection<String> linesSources = new ArrayList(Arrays.asList(model.getOutPut().split("\n")));
+            ((ArrayList<String>) linesSources).remove(0);
+            ((ArrayList<String>) linesSources).remove(0);
+            linesSources.remove("Source files for which symbols will be read in on demand:");
+            linesSources.remove("(gdb) ");
+            for (String ls : linesSources) {
+                String[] filePaths = ls.split(",");
+                for (String fp : filePaths) {
+                    if (fp.length() > 1 && !fp.endsWith(".h")) {
+                        Tab tab = new Tab();
+                        String fileName = fp.substring(fp.lastIndexOf("/") + 1);
+                        Tooltip tt = new Tooltip();
+                        tt.setText(fp);
+                        tab.setTooltip(tt);
+                        tab.setText(fileName);
 
-                    model.setInput("list "+fp+":0");
-                    String out = model.getOutPut();
-                    out = out.substring(0,out.length()-6);
-                    String[] codeLines = out.split("\n");
+                        model.setInput("list " + fp + ":0");
+                        String out = model.getOutPut();
+                        out = out.substring(0, out.length() - 6);
+                        String[] codeLines = out.split("\n");
 
-                    ListView code = new ListView();
-                    java.util.List<String> lines = Arrays.asList(codeLines);
-                    code.setItems(FXCollections.observableList(lines));
-                    code.setCellFactory(CheckBoxListCell.forListView((Callback<String, ObservableValue<Boolean>>) item -> {
-                        BooleanProperty observable = new SimpleBooleanProperty();
-                        observable.addListener((obs, wasSelected, isNowSelected)->
-                                {
-                                    //System.out.println("Check box for "+item+" changed from "+wasSelected+" to "+isNowSelected);
-                                    if(!wasSelected) {
-                                        model.setInput("break " + item.substring(0,item.indexOf('\t')));
+                        ListView code = new ListView();
+                        java.util.List<String> lines = Arrays.asList(codeLines);
+                        code.setItems(FXCollections.observableList(lines));
+                        code.setCellFactory(CheckBoxListCell.forListView((Callback<String, ObservableValue<Boolean>>) item -> {
+                            BooleanProperty observable = new SimpleBooleanProperty();
+                            observable.addListener((obs, wasSelected, isNowSelected) ->
+                                    {
+                                        //System.out.println("Check box for "+item+" changed from "+wasSelected+" to "+isNowSelected);
+                                        if (!wasSelected) {
+                                            model.setInput("break " + item.substring(0, item.indexOf('\t')));
+                                        } else
+                                            model.setInput("clear " + item.substring(0, item.indexOf('\t')));
+                                        model.getOutPut();
                                     }
-                                    else
-                                        model.setInput("clear " + item.substring(0,item.indexOf('\t')));
-                                    model.getOutPut();
-                                }
-                        );
-                        return observable ;
-                    }));
+                            );
+                            return observable;
+                        }));
 
-                    tab.setContent(code);
-                    sourceFiles.getTabs().add(tab);
-                }
-            }
-        }
-        sourceFiles.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-        model.setInput("list "+sourceFiles.getSelectionModel().getSelectedItem().getTooltip().getText()+":0");
-        model.getOutPut();
-
-        sourceFiles.getSelectionModel().selectedItemProperty().addListener(
-                new ChangeListener<Tab>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Tab> ov, Tab oldValue, Tab newValue) {
-                        model.setInput("list "+newValue.getTooltip().getText()+":0");
-                        model.getOutPut();
+                        tab.setContent(code);
+                        sourceFiles.getTabs().add(tab);
                     }
                 }
-        );
+            }
+            sourceFiles.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+            model.setInput("list " + sourceFiles.getSelectionModel().getSelectedItem().getTooltip().getText() + ":0");
+            model.getOutPut();
+
+            sourceFiles.getSelectionModel().selectedItemProperty().addListener(
+                    new ChangeListener<Tab>() {
+                        @Override
+                        public void changed(ObservableValue<? extends Tab> ov, Tab oldValue, Tab newValue) {
+                            model.setInput("list " + newValue.getTooltip().getText() + ":0");
+                            model.getOutPut();
+                        }
+                    }
+            );
+
+            RunBtn.setDisable(false);
+            StepBtn.setDisable(false);
+            StepInBtn.setDisable(false);
+            StepOutBtn.setDisable(false);
+            ContinueBtn.setDisable(false);
+        }
     }
 
     @FXML
@@ -125,6 +149,20 @@ public class GDBController {
         nextMarkedLine();
     }
 
+    @FXML
+    protected void StepOut(ActionEvent event){
+        model.setInput("finish");
+        model.getOutPut();//redundant output
+        nextMarkedLine();
+    }
+
+    @FXML
+    protected void Continue(ActionEvent event){
+        model.setInput("continue");
+        model.getOutPut();//redundant output
+        getFocusedSourceFile().getSelectionModel().select(-1);
+    }
+
     private void nextMarkedLine(){
         model.setInput("info source");
         String out = model.getOutPut();
@@ -144,11 +182,17 @@ public class GDBController {
         String lineNumberString = out.substring(out.indexOf(":")+1,out.indexOf("\n"));
         try {
             lineNumber = Integer.parseInt(lineNumberString) - 1;
-            Tab selectedSourceFileTab = sourceFiles.getSelectionModel().getSelectedItem();
-            ListView code = ((ListView) selectedSourceFileTab.getContent());
-            code.getSelectionModel().select(lineNumber);
+//            Tab selectedSourceFileTab = sourceFiles.getSelectionModel().getSelectedItem();
+//            ListView code = ((ListView) selectedSourceFileTab.getContent());
+//            code.getSelectionModel().select(lineNumber);
+            getFocusedSourceFile().getSelectionModel().select(lineNumber);
         }
         catch (NumberFormatException e) {
         }
+    }
+
+    private ListView getFocusedSourceFile(){
+        Tab selectedSourceFileTab = sourceFiles.getSelectionModel().getSelectedItem();
+        return ((ListView) selectedSourceFileTab.getContent());
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("GDBView.fxml"));
         primaryStage.setTitle("GDB UI");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 640));
         primaryStage.show();
     }
 
